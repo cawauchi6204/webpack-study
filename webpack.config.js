899e8ea6763772a959537167d5f21383e1ee9e68
cawauchi6204 <cawauchi6204@gmail.com>
2020-06-16 09:34:01 +0900

    pugの追加

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,21 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             esModule: false,
-                            name:'images/[name].[ext]',
+                            name: 'images/[emoji][name].[ext]',
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty:true
                         }
                     }
                 ]
@@ -36,7 +50,12 @@ module.exports = {
             filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename:'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename:'access.html'
         }),
         new CleanWebpackPlugin(),
     ]
